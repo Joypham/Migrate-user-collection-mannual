08d60b6b4ac0521994198f45ce382b6ee521561a
Pham Hanh <48047723+Joypham@users.noreply.github.com>
2020-05-18 10:28:51 +0700

    Update Update videos_youtubeurl.sql

--- Update videos_youtubeurl.sql
+++ Update videos_youtubeurl.sql
@@ -1,12 +1,13 @@
 -- Chi update videoid với các videoid chưa có trong bảng videos_youtubeurl
-TRUNCATE TABLE videos_youtubeurl ;
+
+-- TRUNCATE TABLE videos_youtubeurl ;
 
 INSERT INTO videos_youtubeurl (VideoID,SourceId,YoutubeID,filename_reformat)
 
 SELECT 
 
 ID as VideoID,
-SourceId,
+videos.SourceId,
 if(LEFT(right(filename,LENGTH(filename)-8),3)='___',
 SUBSTRING_INDEX(right(filename,LENGTH(filename)-8),"___",2)
 ,
@@ -14,5 +15,10 @@ SUBSTRING_INDEX(right(filename,LENGTH(filename)-8),"___",1)
 ) as YoutubeID,
 filename || '.mp4' as filename_reformat
 from videos
+LEFT JOIN videos_youtubeurl on videos.id = videos_youtubeurl.VideoId
 WHERE 
 videos.SourceId in ('717340540353713','779815522042770')
+and
+videos.Valid =1
+AND
+videos_youtubeurl.VideoId is null
